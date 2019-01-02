af45379c67e2e111b6fb5edf2ff5ee08c5368a0d
Veraprot <vera.protopopova@gmail.com>
2019-01-02 11:40:39 -0500

    filter data for game, make api request only if the record is not stored in database

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -4,9 +4,15 @@ class Api::V1::GamesController < ApplicationController
   @@cards = []
 
   def create_timeline 
-    HistoryAPI.process_request(game_params[:startDate], game_params[:endDate])
-    @cards = Card.all
-    puts @cards
+    earliest_record = Card.minimum(:date)
+    latest_record = Card.maximum(:date)
+
+    if game_params[:startDate] < earliest_record || 
+       game_params[:endDate] > latest_record
+      HistoryAPI.process_request(game_params[:startDate], game_params[:endDate])
+    end 
+
+    @cards = Card.where("date > ? AND date < ?", game_params[:startDate], game_params[:endDate])
     render json: @cards, status: :created
   end 
 
--- app/models/concerns/history_api.rb
+++ app/models/concerns/history_api.rb
@@ -5,7 +5,7 @@ require 'active_support/core_ext/hash'
 
 module HistoryAPI 
   def self.make_request(begin_date, end_date)
-    base_url = "http://www.vizgr.org/historical-events/search.php?begin_date=#{begin_date}&end_date=#{end_date}&limit=900"
+    base_url = "http://www.vizgr.org/historical-events/search.php?begin_date=#{begin_date}&end_date=#{end_date}"
     s = Net::HTTP.get_response(URI.parse(base_url)).body
     end_input = Hash.from_xml(s)
     end_input 
@@ -14,7 +14,7 @@ module HistoryAPI
   def self.process_request(begin_date, end_date)
     result_hash = make_request(begin_date, end_date)
     result_hash["result"]["event"].each do |event|
-      newCard = Card.new(date: event["date"], event: event["description"])
+      newCard = Card.new(date: event["date"].split("/").join(''), event: event["description"])
       if newCard.valid? 
         newCard.save()
       else
